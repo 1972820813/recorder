1c56b781cb90f8d5482c4eb39710e206fdcedf93
Alex.li <1972820813@qq.com>
2024-12-08 18:23:20 +0800

    布局优化

--- lib/model/home/schema/schema_details_page.dart
+++ lib/model/home/schema/schema_details_page.dart
@@ -19,9 +19,43 @@ class SchemaDetailsPage extends GetView<SchemaDetailsPageController> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(
+                height: 40,
+              ),
               customBar(controller.pageTitle.value),
+              _contentView("一、模式简介", "通用模式适用于绝大多数录音场景，如课堂、发布会等。"),
+              const SizedBox(
+                height: 10,
+              ),
+              _contentView("二、麦克风工作", "定向麦克和阵列麦克同时收音，通过算法进行智能降噪优化，录音效果相对均衡。"),
+              const SizedBox(
+                height: 10,
+              ),
+              _contentView("三、摆放建议", "建议将定向麦克对准声源。"),
             ],
           ),
         ));
+  }
+
+  Widget _contentView(String title, String content) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 18, color: DoColors.white85),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+          child: Text(
+            content,
+            style: const TextStyle(fontSize: 18, color: DoColors.gray102),
+          ),
+        ),
+      ],
+    );
   }
 }
